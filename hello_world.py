05fc29031deabee0b5c40c6218c76eff46654d02
MikeTysonson <unkenntlich@gmx.de>
2018-06-11 15:53:00 +0200

    renamed but everything stays the same

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+  print ('Hello World')
+  print ('i already know more than before')
+  print ("now the renaming get's weird")
+  print ('how can git now this is the same file if both name and content changed')
--- hello_worldiiiiii.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-  print ('Hello World')
-  print ('i already know more than before')
