704e020e642077a3df720df9c12f70b862233e1e
MikeTysonson <unkenntlich@gmx.de>
2018-06-11 15:54:32 +0200

    add and commit

--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,4 @@ if __name__ == "__main__":
   print ('i already know more than before')
   print ("now the renaming get's weird")
   print ('how can git now this is the same file if both name and content changed')
+  print ('why do i even add if it all goes in one Aufwasch')
