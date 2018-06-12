42a9c1d3633d3f03078f0fad912405f91288f91c
MikeTysonson <unkenntlich@gmx.de>
2018-06-12 12:49:55 +0200

    Palmen

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,5 @@
+import sys
+
 if __name__ == "__main__":
   print ('Hello World')
   print ('i already know more than before')
@@ -9,3 +11,13 @@ if __name__ == "__main__":
 from monkey  import * 
 
 printSome()
+
+def print_palmtrees(num):
+  for i in range(num):
+    sys.stdout.write(' /|\\')
+  sys.stdout.write('\n')
+  for i in range(num):
+    sys.stdout.write('  | ')
+  sys.stdout.write('\n\n')
+
+print_palmtrees(2)
